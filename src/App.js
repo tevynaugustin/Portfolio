9ea1c8911a2a95b73d3a505d117b85627f2bf3e7
tevynaugustin <tevynaugustin@gmail.com>
2023-04-03 19:42:36 +0100

    added contacts

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Video from './components/video/video';
 import About from './pages/aboutme/aboutme';
 import Experience from './pages/experience/experience';
 import Projects from './pages/projects/projects';
+import Contact from './components/contact/contact';
 
 function App() {
   return (
     <div className="App">
       <Video/>
+      <Contact/>
       <Landing/>
       <About/>
       <Experience/>
---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './contact.css'
+import { faGithub, faLinkedin, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+
+function Contact() {
+  return (
+    <div>
+        <section id="contact">
+            <div className="contactContainer">
+                <ul>
+                    <a href='https://github.com/tevynaugustin'><li className="item"><FontAwesomeIcon className='icon' icon={faGithub} size='2x'/></li></a>
+                    <a href='https://www.linkedin.com/in/tevyna/'><li className="item"><FontAwesomeIcon className='icon' icon={faLinkedin} size='2x'/></li></a>
+                    <a href='https://www.instagram.com/tiijjaayyy/?next=%2F'><li className="item"><FontAwesomeIcon className='icon' icon={faSquareInstagram} size='2x'/></li></a>
+                </ul>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Contact
