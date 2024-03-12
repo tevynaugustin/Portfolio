ba984372b6141252cd7fca81f59643b482c87b3d
Tevyn Augustin <tevynaugustin@gmail.com>
2024-03-12 10:56:34 +0000

    updated experience

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -13,13 +13,13 @@ function Experience() {
 
         <AnimationOnScroll animateIn="animate__backInDown">
           <div className="flex">
-          <div className="jobContainer">
-            <h2 className="jobTitle">
-              PEP (Paragon E-Commerce Platform) Developer @{" "}
-              <span className="highlight">Paragon</span>
-            </h2>
-            <h4 className="expDate">March 2022 - Present</h4>
-            
+            <div className="jobContainer">
+              <h2 className="jobTitle">
+                PEP (Paragon E-Commerce Platform) Developer @{" "}
+                <span className="highlight">Paragon</span>
+              </h2>
+              <h4 className="expDate">March 2022 - Present</h4>
+
               <ul className="descList">
                 <li className="descItem">
                   <h4>
@@ -29,26 +29,27 @@ function Experience() {
                   </h4>
                 </li>
               </ul>
-            
-          </div>
-          
-          <div className="jobContainer">
-            <h2 className="jobTitle">
-              Website Manager @ <span className="highlight">Ace Interiors</span>
-            </h2>
-            <h4 className="expDate">November 2022 - Present</h4>
-            
+            </div>
+
+            <div className="jobContainer">
+              <h2 className="jobTitle">
+                Website Manager @{" "}
+                <span className="highlight">Ace Interiors</span>
+              </h2>
+              <h4 className="expDate">November 2022 - Present</h4>
+
               <ul className="descList">
                 <li className="descItem">
                   <h4>
-                    I lead in designing and implementing innovative software
-                    solutions, seamlessly integrating e-commerce functionality
-                    with our robust content management system.
+                    (ace-interiors.co.uk) - I took on a challenging project of
+                    helping a company transition its website from their old
+                    hosting provider to a new platform. It was my first time
+                    tackling such a task, and it was quite a learning
+                    experience.
                   </h4>
                 </li>
               </ul>
-            
-          </div>
+            </div>
           </div>
         </AnimationOnScroll>
       </section>
